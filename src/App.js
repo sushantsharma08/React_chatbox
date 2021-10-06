7141dfbbfc8e552955dd13b387588188c700fdd6
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2021-10-06 23:35:13 +0530

    latest commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import { ChatEngine} from 'react-chat-engine';
+import { ChatEngine } from 'react-chat-engine';
+
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = '090ec70b-b303-40a6-835b-8ed4f6ee5b76';
+
 const App = () => {
-    return (
-        <ChatEngine
-          height="100vh"
-          projectID="090ec70b-b303-40a6-835b-8ed4f6ee5b76"
-          userName="SushantSharma"
-          userSecret="sharmasu"
-          renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps} />}
-        />
-        
-    );
-    
-}
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
 
 export default App;
