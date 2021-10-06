40f2dea88c60404fe61d3f09c1110caced5d3248
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2021-10-06 09:10:16 +0530

    app:messages

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,5 +1,27 @@
+import MessageForm from "./MessageForm";
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMEssage";
 const ChatFeed = (props) => {
-        console.log(props);
+        const { chats, activeChat, userName, messages } = props;
+
+        const chat = chats && chats[activeChat];
+        
+        const renderMessages = () => {
+            const keys = Object.keys(messages);
+
+            return keys.map(key,index) => {
+                const message = messages[key];
+                const lastMessageKey = index === 0 ? null : keys[index-1];
+                const isMyMessage = userName  message.sender.username;
+
+                return (
+                    <div key={`msg`}>
+
+                    </div>
+                );
+
+            }
+        }
     return(
         <div>
             ChatFeed
